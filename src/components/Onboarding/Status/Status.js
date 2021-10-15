7b88c71fb002f0c4caabc29153c0b191a91bcf5a
Roman Shprenger <shprengerroman@gmail.com>
2021-10-16 01:12:40 +0300

    Added progress bar indicator

--- src/components/Onboarding/Status/Status.js
+++ src/components/Onboarding/Status/Status.js
@@ -5,6 +5,8 @@ import {
 } from '@services/Onboarding/onboardingSlice';
 import styles from './Status.module.scss';
 
+import {ReactComponent as CheckmarkIcon}  from '@assets/icons/checkmark-outline.svg'
+
 const steps = [
   { title: 'Check Past Rewards' },
   { title: 'Create Rosetta Wallet' },
@@ -15,11 +17,28 @@ const steps = [
 const Status = () => {
   const step = useSelector(selectStep);
 
+  const condition = (i) => {
+    // Strong logic of progress bar with multisteps inside of steps
+    if (1 <= step && step < 6 && i === 0) {
+      return true;
+    } else if (6 <= step && step < 9 && i <= 1) {
+      return true;
+    } else if (step >= 9 && i <= 2) {
+      return true
+    } else {
+      return false;
+    }
+  }
+
   return <div className={styles.progress} data-step={step}>
     {
-      steps.map(step => <div className={styles.step} key={step.title}>
-        <span className={styles.dot} />
-        <div className={styles.title}>{step.title}</div>
+      steps.map((stepsItem, index) => <div className={`${styles.step} ${condition(index) ? styles.done : ''}`} key={stepsItem.title}>
+        <span className={styles.dot}>
+          {
+            condition(index) ? <span><CheckmarkIcon/></span> : <span>{index + 1}</span>
+          }
+        </span>
+        <div className={styles.title}>{stepsItem.title}</div>
       </div>)
     }
   </div>;
