cb7605a8e1dc97cf5fecc296fb4574e380f150cb
Shahab Lighari <sklaghari77@gmail.com>
2021-11-22 22:30:39 +0500

    searching common authors

--- src/components/Search/SearchUsers/SearchUsers.js
+++ src/components/Search/SearchUsers/SearchUsers.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import styles from './SearchUsers.module.scss';
 import { FindCommonAuthors } from '../../../utils/common';
-import {dummySearchResults} from './dummydata';
 import { useSelector } from 'react-redux';
 import {selectSearchResult} from '@services/Search/searchSlice';
 
