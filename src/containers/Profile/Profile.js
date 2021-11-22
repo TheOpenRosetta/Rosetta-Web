c226a10e661397d5bfe3b7305cc7a8cea30cf5b8
dpk8990 <Deepkumarss016@gmail.com>
2021-11-22 18:48:14 +0530

    ui works start for profile page

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -16,19 +16,26 @@ import {
   selectUserData,
   fetchUser
 } from '@services/User/userSlice';
+import {
+  //SearchFilters,
+  SearchResults,
+  SearchUsers
+} from '@components/Search';
 
 import month from '@dataset/month.json';
 import half from '@dataset/half.json';
 import three from '@dataset/three.json';
 import year from '@dataset/year.json';
 
-import {ReactComponent as FollowersIcon} from '@assets/customIcons/customer.svg';
-import {ReactComponent as FollowingIcon} from '@assets/customIcons/followers.svg';
-import {ReactComponent as CommunityIcon} from '@assets/customIcons/community.svg';
+import { ReactComponent as FollowersIcon } from '@assets/customIcons/customer.svg';
+import { ReactComponent as FollowingIcon } from '@assets/customIcons/followers.svg';
+import { ReactComponent as CommunityIcon } from '@assets/customIcons/community.svg';
 
 import AvatarImg from '@assets/avatar.png';
 import 'react-tabs/style/react-tabs.css';
 import styles from './Profile.module.scss';
+import { ReactComponent as EditIcons } from '@assets/icons/edit_profile.svg';
+
 
 resetIdCounter();
 
@@ -41,6 +48,13 @@ const Profile = () => {
   const dispatch = useDispatch();
   const [key, setKey] = useState('month');
   const [data, setData] = useState([]);
+  const [ShowEditModal, setShowEditModal] = useState(false);
+  
+  // function for making number digits to k
+  function kFormatter(num) {
+    return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
+  }
+  
 
   useEffect(() => {
     switch (key) {
@@ -63,11 +77,17 @@ const Profile = () => {
     dispatch(fetchUser({ username }));
   }, [dispatch, username]);
 
+  // Open and Hide modal
+  const activateEditModal = () => {
+    console.log("!ShowEditModal", !ShowEditModal);
+    setShowEditModal(!ShowEditModal);
+  }
+
   if (status !== 'loaded') {
     return <div className={styles.profile}>
       <Header className={styles.profileHeader} />
       <div className={styles.profileTop}>
-        <div className={styles.loading}><Loader/></div>
+        <div className={styles.loading}><Loader /></div>
       </div>
     </div>
   }
@@ -84,7 +104,7 @@ const Profile = () => {
             <div className={styles.name}>{userData.name}</div>
             <div className={styles.status}>{userData.status}</div>
             <div className={styles.committed}>Academic Fraud Committed: <span>{userData.fraudCommitted ? 'yes' : 'no'}</span></div>
-            <div className={styles.impact}>ImpactScore: <span>{userData.impactScore}</span></div>
+            <div className={styles.impact}>ImpactScore: <span>{kFormatter(userData.impactScore)}</span></div>
             <div className={styles.infoActions}>
               <Button classes={styles.btnFollow} type="button" size="md" kind="fill">Follow</Button>
               <Button classes={styles.btnSponsor} type="button" size="md" kind="outline">Sponsor</Button>
@@ -113,12 +133,14 @@ const Profile = () => {
     <Tabs className={styles.tabs} selectedTabClassName={styles.tabActive} defaultIndex={tabIndex} onSelect={index => setTabIndex(index)}>
       <div className={styles.profileBar}>
         <div className="container">
-          <div className={styles.main}>
+          <div className={styles.profileMain}>
             <TabList className={styles.tabsList}>
               <Tab className={styles.tab}>Overview</Tab>
               <Tab className={styles.tab}>Papers</Tab>
               <Tab className={styles.tab}>Portfolio</Tab>
+              <Tab className={styles.tab}>Comments</Tab>
             </TabList>
+            <Button classes={styles.btnEdit} type="button" size="md" kind="fill" onClick={() => activateEditModal()}>Edit profile</Button>
           </div>
         </div>
       </div>
@@ -127,20 +149,21 @@ const Profile = () => {
           <div className={styles.stats}>
             <div className={styles.statsItem}>
               <FollowersIcon />
-              {userData.stats.followers}
+              {kFormatter(userData.stats.followers)}
               <span>followers</span>
             </div>
             <div className={styles.statsItem}>
               <FollowingIcon />
-              {userData.stats.following}
+              {kFormatter(userData.stats.following)}
               <span>following</span>
             </div>
             <div className={styles.statsItem}>
               <CommunityIcon />
-              {userData.stats.communities}
+              {kFormatter(userData.stats.communities)}
               <span>communities</span>
             </div>
           </div>
+          <div className={styles.sectionTitleAwards}>Awards</div>
         </div>
         <div className={styles.main}>
           <TabPanel className={styles.tabPanel}>
@@ -164,7 +187,7 @@ const Profile = () => {
               <div className={styles.sectionTitle}>Papers</div>
               <div className={styles.papersContent}>
                 {
-                  // Add PaperPreview Component in loop
+                  <SearchResults />
                 }
               </div>
             </div>
@@ -185,9 +208,65 @@ const Profile = () => {
               </div>
             </div>
           </TabPanel>
+          <TabPanel className={styles.tabPanel}>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>Comments</div>
+              <div className={styles.papersContent}>
+                {
+                  // Add PaperPreview Component in loop
+                }
+              </div>
+            </div>
+          </TabPanel>
         </div>
       </div>
     </Tabs>
+
+    {ShowEditModal
+      ? <div className={styles.editProfileModal}>
+        <div className={styles.editProfileModalInner}>
+          <div className={styles.editProfileModalContent}>
+            <div className={styles.editProfileModalHeader}>
+              <div className={styles.editProfileModalHeaderTitle}>Edit profile</div>
+              <Button classes={styles.btnEdit} type="button" size="md" kind="fill">Save</Button>
+            </div>
+            <div className={styles.editProfileModalBody}>
+              <div className={styles.editProfileModalBodyAvatarr}>
+
+                <Avatar src={userData.AvatarImg || AvatarImg} size="l" />
+
+                <EditIcons />
+
+              </div>
+              <form className={styles.editProfileModalForm}>
+                <div className={styles.editProfileModalFormGroup}>
+                  <input
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    required
+                    className={styles.field}
+                  />
+                  <label htmlFor="firstName" className={styles.label}>First Name</label>
+                </div>
+                <div className={styles.editProfileModalFormGroup}>
+                  <textarea
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    required
+                    className={styles.textarea}></textarea>
+                  <label htmlFor="firstName" className={styles.label}>Bio</label>
+                </div>
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      : ""
+    }
+
   </div>
 }
 
